907ea5649a345c7aa7f7b2b2eef2273a3173b02f
Braden <fish2355@hotmail.com>
2020-02-07 13:18:01 -0700

    commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,7 @@
 axios
   .get("https://api.github.com/users/1sdc0d3r")
   .then(function(res) {
-    // console.log(res.data);
+    console.log(res);
     cardCreator(res.data);
     // followersArray.push(res.data);
     // console.log(followersArray);
@@ -17,6 +17,7 @@ function cardCreator(data) {
 
   const img = document.createElement("img");
   img.src = data.avatar_url;
+  // img.setAttribute("src", data.avatar_url);
 
   const cardInfo = document.createElement("div");
   cardInfo.classList.add("card-info");
@@ -44,10 +45,18 @@ function cardCreator(data) {
   profileLink.textContent = data.html_url;
 
   const followers = document.createElement("p");
-  followers.textContent = `Followers: ${data.followers}`;
+  if (data.followers === 0) {
+    followers.textContent = `Followers: No Friends :_(`;
+  } else {
+    followers.textContent = `Followers: ${data.followers}`;
+  }
 
   const following = document.createElement("p");
-  following.textContent = `Following: ${data.following}`;
+  if (data.following === 0) {
+    following.textContent = `Following: Lone Wolf`;
+  } else {
+    following.textContent = `Following: ${data.following}`;
+  }
 
   const bio = document.createElement("p");
   if (data.bio === null) {
@@ -64,71 +73,42 @@ function cardCreator(data) {
 }
 
 let followersArray = [];
-let cardArray = [];
+// let cardArray = [];
+
+// axios
+//   .get("https://api.github.com/users/1sdc0d3r/followers")
+//   .then(function(res) {
+//     console.log(res);
+//     res.data.forEach(function(e) {
+//       axios
+//         .get(e.url)
+//         .then(res => {
+//           // console.log(res);
+//           cardCreator(res.data);
+//         })
+//         .catch(err => console.log(err));
+//     });
+//   })
+//   .catch(err => console.log(err));
 
 axios
   .get("https://api.github.com/users/1sdc0d3r/followers")
   .then(function(res) {
-    console.log(res.data);
+    console.log(res);
     res.data.forEach(function(e) {
-      // followersArray.push(e);
-      // console.log(e);
-      cardCreator(e);
+      followersArray.push(e.login);
     });
-    // console.log(followersArray);
+  })
+  .then(function(res) {
+    console.log(followersArray);
+    followersArray.forEach(function(e) {
+      axios
+        .get(`https://api.github.com/users/${e}`)
+        .then(res => {
+          console.log(res);
+          cardCreator(res.data);
+        })
+        .catch(err => console.log(err));
+    });
   })
   .catch(err => console.log(err));
-
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
-
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
